01a76537a150fe95e17677e421d161cf5e42b043
brindharajathirajan <97247977+brindharajathirajan@users.noreply.github.com>
2023-10-01 22:55:20 +0530

    Valid Palindrome.java

---
+++ Valid Palindrome.java
@@ -0,0 +1,15 @@
+// Problem link - https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+        int i = 0;
+        int j=  s.length() - 1;
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;j--;
+        }
+        return true;
+    }
+}
+
